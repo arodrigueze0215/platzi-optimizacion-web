eddc29bd5e6aa3e5065bfb3834031ea78b59a87b
arodrigueze0215 <arodriguez@veritran.com>
2020-11-23 19:43:25 -0500

    optimize lodash package use

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const webpack = require('webpack')
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const shouldAnalyze = process.argv.includes('--analyze')
 const plugins = []
 if (shouldAnalyze) {
   plugins.push(new BundleAnalyzerPlugin())
 }
-const nodeEnv = process.env.NODE_ENV || 'development'
-
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
+const nodeEnv = process.env.NODE_ENV || 'development'.BundleAnalyzerPlugin
 
 const config = {
   mode: nodeEnv,
